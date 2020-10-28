1e452fbf0bfe3078d505fe58d4574f4e13f14815
Emmanuel Gava <emmanuelgava@hotmail.com>
2020-10-27 23:15:55 -0300

    actualizacion

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -40,6 +40,7 @@ $$(document).on("page:init", '.page[data-name="about"]', function (e) {
   // Do something here when page with data-name="about" attribute loaded and initialized
 
   var jugadores = 2;
+  var ele1 = 0;
 
   for (var i = 1; i <= jugadores; i++) {
     //Agrego un div en formato boton con el id jugadorNro para identificar el cabezal de la columna
@@ -48,7 +49,7 @@ $$(document).on("page:init", '.page[data-name="about"]', function (e) {
         i +
         '" class="button button-outline"></a></div>'
     );
-    //Agrego un div con el id dado para identificar cada boton hasta el 6 (dado1 + i = dado 11, dado2+i = dado21) 
+    //Agrego un div con el id dado para identificar cada boton hasta el 6 (dado1 + i = dado 11, dado2+i = dado21)
     $$("#nJug" + i).text($$("#jugador" + i).val());
     $$("#uno").append(
       '<div class="col-33">' +
@@ -137,86 +138,180 @@ $$(document).on("page:init", '.page[data-name="about"]', function (e) {
         i +
         '" class="button button-raised"> 0 </a></div>'
     );
-}
-// recorro con dos for para darle funcionalidad a todos los botones
-for (v = 1; v <= jugadores;v++){
-for(i=1; i <=6;i++){
-$$("#dado"+i + v).on("click", function(){
-  ac1.open();
-  console.log("estoy aca");
-  
+  }
+  // recorro con dos for para darle identicacion a los botones del 1 al 6 de cada equipo.
+  for (v = 1; v <= jugadores; v++) {
+    for (i = 1; i <= 6; i++) {
+      $$("#dado" + i + v).on("click", function () {
+        traerValoresDado(this.id);
+      });
+    }
+  }
+
+  function traerValoresDado(nombre) {
+    //Traigo el id atraves de nombre y agarro con ele1 la posicion 5 (porque el array arranca en cero) que es el numero que identifica a cada div
+    var ele1 = parseInt(nombre[4]);
+
+    var ac1 = app.actions.create({
+      buttons: [
+        {
+          text: "dado " + ele1,
+        },
+        {
+          text: "Uno",
+          onClick: function () {
+            // PENSE EN BUSCAR LA POSICION 5 (VENDRIA A SER DE DADO1 EL 1 PARA PODER MULTIPLICARLO POR EL NUMERO DE BOTON PRESIONADO)
+            //ACA TRATANDO DE OBTENER LA POSICION 5 , PERO NO FUNCIONA.
+            totalCuadrante = 1 * ele1;
+            console.log(totalCuadrante); //ACA SOLO ESTABA PROBANDO SI ele1 me daba el resultado que queria.
+            $$("#" + nombre).text(totalCuadrante);
+          },
+        },
+        {
+          text: "Dos",
+          onClick: function () {
+            totalCuadrante = 2 * ele1;
+            console.log(totalCuadrante);
+            $$("#" + nombre).text(totalCuadrante);
+          },
+        },
+        {
+          text: "Tres",
+          onClick: function () {
+            totalCuadrante = 3 * ele1;
+            console.log(totalCuadrante);
+            $$("#" + nombre).text(totalCuadrante);
+          },
+        },
+        {
+          text: "Cuatro",
+          onClick: function () {
+            totalCuadrante = 4 * ele1;
+            console.log(totalCuadrante);
+            $$("#" + nombre).text(totalCuadrante);
+          },
+        },
+        {
+          text: "Cinco",
+          onClick: function () {
+            totalCuadrante = 5 * ele1;
+            console.log(totalCuadrante);
+            $$("#" + nombre).text(totalCuadrante);
+          },
+        },
+        {
+          text: "Seis",
+          onClick: function () {
+            totalCuadrante = 6 * ele1;
+            console.log(totalCuadrante);
+            $$("#" + nombre).text(totalCuadrante);
+          },
+        },
+        {
+          text: "Tachar",
+          onClick: function () {
+            totalCuadrante = 0 * ele1;
+            console.log(totalCuadrante);
+            $$("#" + nombre).text("X");
+          },
+        },
+        {
+          text: "Cancel",
+          color: "red",
+        },
+      ],
+    });
+
+    ac1.open();
+  }
+
+  for (i = 1; i <= jugadores; i++) {
+    $$("#escalera" + i).on("click", function () {
+      traerValoresJuego(this.id, "escalera");
+    });
+    $$("#full" + i).on("click", function () {
+      traerValoresJuego(this.id, "full");
+    });
+    $$("#poker" + i).on("click", function () {
+      traerValoresJuego(this.id, "poker");
+    });
+    $$("#generala" + i).on("click", function () {
+      traerValoresJuego(this.id, "generala");
+    });
+    $$("#generalaDirecta" + i).on("click", function () {
+      traerValoresJuego(this.id, "generalaDirecta");
+    });
+
+    function traerValoresJuego(nombre, tipo) {
+      var ac1 = app.actions.create({
+        buttons: [
+          {
+            text: "Juego",
+          },
+          {
+            text: "Servido",
+            onClick: function () {
+              switch (tipo) {
+                case "escalera":
+                  totalCuadrante = 25;
+                  break;
+                case "full":
+                  totalCuadrante = 35;
+                  break;
+                case "poker":
+                  totalCuadrante = 45;
+                  break;
+                case "generala":
+                  totalCuadrante = 55;
+                  break;
+                case "generalaDirecta":
+                  totalCuadrante = 65;
+                  break;
+                default:
+              }
+              console.log(totalCuadrante);
+              $$("#" + nombre).text(totalCuadrante);
+            },
+          },
+          {
+            text: "No Servido",
+            onClick: function () {
+              switch (tipo) {
+                case "escalera":
+                  totalCuadrante = 20;
+                  break;
+                case "full":
+                  totalCuadrante = 30;
+                  break;
+                case "poker":
+                  totalCuadrante = 40;
+                  break;
+                case "generala":
+                  totalCuadrante = 50;
+                  break;
+                case "generalaDirecta":
+                  totalCuadrante = 60;
+                  break;
+                default:
+              }
+              $$("#" + nombre).text(totalCuadrante);
+            },
+          },
+          {
+            text: "Tachar",
+            onClick: function () {
+              totalCuadrante = 0;
+              $$("#" + nombre).text("X");
+            },
+          },
+          {
+            text: "Cancel",
+            color: "red",
+          },
+        ],
+      });
+
+      ac1.open();
+    }
+  }
 });
-
-}
-}
-
-var ac1 = app.actions.create({
-  
-  //ACA ESTOY TRABADO , QUIERO OBTENER EL ID DE CADA BOTON PRESIONADO Y ...
-  buttons: [
-    {
-      text: " ", // YA TENDRIA QUE TENERLO DE ANTEMANO PARA AGREGARLO AL TITULO DE POP-UP SEGUN CORRESPONDA SU ID.
-    },
-    {
-      text: 'Uno',
-      onClick: function () {
-        var arr = document.getElementById(this);
-        // PENSE EN BUSCAR LA POSICION 5 (VENDRIA A SER DE DADO1 EL 1 PARA PODER MULTIPLICARLO POR EL NUMERO DE BOTON PRESIONADO)
-        var ele1= parseInt(arr[5]); //ACA TRATANDO DE OBTENER LA POSICION 5 , PERO NO FUNCIONA.
-        totalCuadrante = 1*2;
-        console.log(totalCuadrante, ele1);//ACA SOLO ESTABA PROBANDO SI ele1 me daba el resultado que queria.
-      }
-
-    },
-    {
-      text: 'Dos',
-      onClick: function () {
-        
-        totalCuadrante = 2*2;
-        console.log(totalCuadrante);
-      }
-      
-    },
-    {
-      text: 'Tres',onClick: function () {
-        
-        totalCuadrante = 3*2;
-        console.log(totalCuadrante);
-      }
-      
-    },
-    {
-      text: 'Cuatro',
-      onClick: function () {
-        
-        totalCuadrante = 4*2;
-        console.log(totalCuadrante);
-      }
-    },
-    {
-      text: 'Cinco',
-      onClick: function () {
-        
-        totalCuadrante = 5*2;
-        console.log(totalCuadrante);
-      }
-    },{
-      text: 'Seis',
-      onClick: function () {
-        
-        totalCuadrante = 6*2;
-        console.log(totalCuadrante);
-      }
-    },
-    {
-      text: 'Cancel',
-      color: 'red'
-    },
-  ]
-})
-});
-
-
-
-  
-
